ed309cd818efb8a235a25ea436d8084055ce5bb9
rosatis-community <rosatis.community@gmail.com>
2021-08-15 19:49:52 +0300

    heroku init

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
